1fae7374c02f2ca9de7e394ddd508e5b7f1a33d2
Leo Zhang <leozhang2024@gmail.com>
2025-04-05 11:09:06 -0700

    improved homepage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -9,23 +9,30 @@ export default function Home() {
       <Header />
       <ChatBot />
 
-      <div className={styles.container}>
-        <div className={styles.main}>
-          <div className={styles.hero}>
-            <h1 className={styles.title}>Welcome to creg</h1>
-            <p className={styles.subtitle}>
-              Your central hub for all course registration needs
-            </p>
-            <div className={styles.ctas}>
-              <Link href="/first-years" className={styles.primaryCta}>
-                First Year Guide
-              </Link>
-              <Link href="/register" className={styles.secondaryCta}>
-                Register for Courses
-              </Link>
+      <div className={styles.heroSection}>
+        <img src="USFCABackground.jpg" alt="Background" className={styles.heroImage} />
+        <div className={styles.heroOverlay}>
+        <div className={styles.container}>
+          <div className={styles.main}>
+            <div className={styles.hero}>
+                <h1 className={`${styles.title} ${styles.animateFadeDrop} ${styles["delay-1"]}`}>
+                  Welcome to creg
+                </h1>
+                <p className={`${styles.subtitle} ${styles.animateFadeDrop} ${styles["delay-2"]}`}>
+                  Your central hub for all course registration needs
+                </p>
+              <div className={styles.ctas}>
+                <Link href="/first-years" className={styles.primaryCta}>
+                  First Year Guide
+                </Link>
+                <Link href="/register" className={styles.secondaryCta}>
+                  Register for Courses
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+          </div>
+          </div>
       </div>
     </>
   );
