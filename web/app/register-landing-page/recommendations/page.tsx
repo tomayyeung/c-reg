5b2485eb8ee30b30a610fc93d97bcf895d4e5bb6
nishoof <88177986+nishoof@users.noreply.github.com>
2025-04-05 21:30:19 -0700

    Add initial course recommendation plan & folder
    Co-authored-by: Leo Zhang <leozhang2024@gmail.com>

---
+++ web/app/register-landing-page/recommendations/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import ChatBot from "@/components/ChatBot";
+import styles from "./page.module.css";
+
+export default function Recommendations() {
+  return (
+    <>
+      <Header />
+      <ChatBot />
+
+
+    </>
+  );
+}
